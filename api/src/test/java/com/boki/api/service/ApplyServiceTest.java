7d1923267ed992cf6bc40a1544101a2baf84ec2d
boki <lsb530@naver.com>
2024-04-08 03:34:35 +0900

    여러명 응모 테스트

--- api/src/test/java/com/boki/api/service/ApplyServiceTest.java
+++ api/src/test/java/com/boki/api/service/ApplyServiceTest.java
@@ -5,10 +5,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ApplyServiceTest {
+
     @Autowired
     private ApplyService applyService;
 
@@ -22,6 +27,30 @@ class ApplyServiceTest {
         long couponCnt = couponRepository.count();
 
         assertEquals(1, couponCnt);
+    }
+
+    @Test
+    public void 여러명응모() throws InterruptedException {
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(userId);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        long couponCnt = couponRepository.count();
+
+        assertEquals(100, couponCnt);
     }
 
 }
